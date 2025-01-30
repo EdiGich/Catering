e8fd81756786ea653e825aec0b905faf87586c0a
Edwin Gichira <edwingichira801@gmail.com>
2025-01-30 19:46:46 +0300

    Allow dt-caterers.onrender.com Host

--- catering_website/catering_website/settings.py
+++ catering_website/catering_website/settings.py
@@ -183,7 +183,7 @@ CORS_ALLOW_ALL_ORIGINS = True  # For development; restrict this for production
 
 #will delete these after environ starts working
 SECRET_KEY='django-insecure-u(tgga&l^2^@pz89ez@rgz7zxg9=#!+#@!cqr!jzgms*f+--6s'
-ALLOWED_HOSTS=['127.0.0.1', 'localhost', '192.168.0.105', '10.0.2.2','catering_website.onrender.com']
+ALLOWED_HOSTS=['127.0.0.1', 'localhost', '192.168.0.105', '10.0.2.2','dt-caterers.onrender.com']
 DEBUG=True
 
 
