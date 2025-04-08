c43c542e80a2e09ca1c1ecd5a12485cd308c0712
Edwin Gichira <edwingichira801@gmail.com>
2025-04-08 15:56:10 +0300

    Automatically Delete Files on Model Deletion

--- catering_website/main/models.py
+++ catering_website/main/models.py
@@ -1,14 +1,23 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_delete
+import os
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='menu_images/', null=True, blank=True)  # Add image field
-    description = models.TextField(blank=True)  # New field
+    description = models.TextField(blank=True) 
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)  
 
     
     def __str__(self):
         return self.name
+    
+@receiver(post_delete, sender=MenuItem)
+def delete_image_file(sender, instance, **kwargs):
+    # Delete the image file from the filesystem when the MenuItem is deleted
+    if instance.image and os.path.isfile(instance.image.path):
+        os.remove(instance.image.path)
 
 class GalleryItem(models.Model):
     title = models.CharField(max_length=100)
